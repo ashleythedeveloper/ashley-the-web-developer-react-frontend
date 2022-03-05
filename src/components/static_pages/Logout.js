1d2cfc213d8bbe0509ab2fd5a652758dbe9f4481
ashleythewebdeveloper <ashley@harwoodslawncare.com.au>
2022-03-05 17:51:22 +1100

    Added the loading screen to the logout component. When the user clicks the logout button it will show the loading screen while the logout component is making the request.

--- src/components/static_pages/Logout.js
+++ src/components/static_pages/Logout.js
@@ -1,31 +1,50 @@
-import React, { useEffect } from 'react';
-import {useHistory} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import axiosInstance from '../global/axiosInstance';
+import Loader from '../global/Loader';
 
 
 const Logout = () => {
   const history = useHistory();
 
+  // Initialise the default loading state
+  const defaultLoadingState = { isLoading: false, loadingMessage: 'Logging you out. One moment.' }
+
+  // Initialise a state variable to controll the loading screen.
+  const [loading, setLoading] = useState(defaultLoadingState);
+
+
   useEffect(() => {
-    
-    axiosInstance.post('auth/blacklist-token/', {}, {withCredentials:true})
-    .then((res) => {
-      history.push('/login')
-    })
-    .catch((err) => {
-      history.push('/login', {
-        notification: {
-          notificationType: 'error',
-          notificationTitle: '',
-          notificationMessage: "You're already logged out.",
-          notificationTimer: null
-        }})
-    })
-    });
-  return (
-    <>
-    </>
-  )
+    setLoading({...loading, isLoading: true});
+    axiosInstance.post('auth/blacklist-token/', {}, { withCredentials: true })
+      .then((res) => {
+        setLoading(defaultLoadingState);
+        history.push('/login')
+      })
+      .catch((err) => {
+        setLoading(defaultLoadingState);
+        history.push('/login', {
+          notification: {
+            notificationType: 'error',
+            notificationTitle: '',
+            notificationMessage: "You're already logged out.",
+            notificationTimer: null
+          }
+        })
+      })
+  });
+  if (loading.isLoading) {
+    return (
+      <>
+        <Loader message={loading.loadingMessage} />
+      </>
+    )
+  } else {
+    return (
+      <>
+      </>
+    )
+  }
 }
 
 export default Logout
