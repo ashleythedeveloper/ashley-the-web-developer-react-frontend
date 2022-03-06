1a93de22579ae106cddba4e5f3e8ccc3b8d9e51c
ashleythewebdeveloper <ashley@harwoodslawncare.com.au>
2022-03-06 12:40:25 +1100

    Changes made to retrieve and display project data

--- src/components/static_pages/PortfolioProject.js
+++ src/components/static_pages/PortfolioProject.js
@@ -5,63 +5,32 @@ import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Chip from '@material-ui/core/Chip';
 import MetaTags from 'react-meta-tags';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
 import Header from '../global/Header';
 import Modal from '@material-ui/core/Modal';
 import Button from '@material-ui/core/Button';
 import Footer from '../global/Footer';
 import {useParams} from 'react-router-dom';
 import Loader from '../global/Loader';
+import axiosInstance from '../global/axiosInstance';
+import ImageList from '@material-ui/core/ImageList';
+import ImageListItem from '@material-ui/core/ImageListItem';
+import Dialog from '@material-ui/core/Dialog';
+
 
 
 
 
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-        position: 'absolute',
-        width: '55%',
-        height: '65%',
-        backgroundColor: theme.palette.background.paper,
-        border: 'none',
-        boxShadow: theme.shadows[5],
-        outline: 'none',
-        padding: theme.spacing(2, 4, 3),
-        [theme.breakpoints.down('sm')]: {
-            width: '75%',
-            height: '45%',
-            maxWidth: 650,
-            maxHeight: 450,
-        },
-        [theme.breakpoints.up('md')]: {
-            width: '75%',
-            height: '45%',
-            maxWidth: 800,
-            maxHeight: 500,
-        },
-        [theme.breakpoints.up('lg')]: {
-            width: '75%',
-            height: '45%',
-            minWidth: 700,
-            minHeight: 500,
-            maxWidth: 900,
-            maxHeight: 500,
-        },
-        [theme.breakpoints.up('xl')]: {
-            width: '55%',
-            height: '45%',
-            maxWidth: 900,
-            maxHeight: 500,
-        },
-      },
-    media:{
-        width: '100%',
-        height: '100%',
+    cardContainer: {
+        marginTop: theme.spacing(5),
+        paddingBottom: theme.spacing(10),
+        borderRadius: 20,
+        overflow: 'visible'
     },
     header: {
         paddingTop: theme.spacing(15),
-        paddingBottom: theme.spacing(5),
+        paddingBottom: theme.spacing(3),
     },
     chips: {
         marginRight: theme.spacing(1),
@@ -77,156 +46,144 @@ const useStyles = makeStyles((theme) => ({
         marginRight: theme.spacing(2),
     },
     imageGrid: {
+        paddingLeft: theme.spacing(2),
+        paddingRight: theme.spacing(2),
         paddingTop: theme.spacing(10),
-        justifyContent: 'center',
-        align: 'center',
+
     },
     button: {
         marginTop: theme.spacing(4),
+        marginRight: 15
     },
     photosHeading: {
         paddingTop: theme.spacing(15),
-    }
+    },
+   
 
 
 }));
 
-function getModalStyle() {
-    const top = 50;
-    const left = 50;
-  
-    return {
-      top: `${top}%`,
-      left: `${left}%`,
-      transform: `translate(-${top}%, -${left}%)`,
-    };
-  }
 
-const PortfolioProject = ({apiData}) => {
+const PortfolioProject = () => {
 
-    const defaultLoadingState = {isLoading: true, loadingMessage: 'Fetching the project. One moment.'}
+    const defaultLoadingState = {isLoading: true, loadingMessage: 'Fetching the project. One moment.'};
 
-    const [loading, setLoading] = useState(defaultLoadingState)
+    const [loading, setLoading] = useState(defaultLoadingState);
+
+
+    const [projectData, setProjectData] = useState();
 
     const {project} = useParams();
 
+    useEffect(() => {
+        axiosInstance.post('project/', {slug: project})
+        .then((res) => {
+            console.log(res.data)
+            setProjectData(res.data);
+            setLoading({...loading, isLoading: false})
+        })
+        .catch((err) => {
+            console.log(err)
+            setLoading({...loading, isLoading: false})
+        })
+    }, [])
 
-    const pageData = {
-        title: "Ashley The Web Developer | Portfolio Project | " + apiData.apiData.prj[0].projectName,
-        description: apiData.apiData.prj[0].projectDescription,
-        url: 'https://ashleythewebdeveloper.com.au'+apiData.match.url,
-        image: '',
-    };    
+
     
     const classes = useStyles()
 
-    const [modalStyle] = React.useState(getModalStyle);
     const [open, setOpen] = React.useState(false);
     const [imageSrc, setImageSrc] = React.useState('/');
 
     const handleOpen = (imageSrc) => {
-        if (window.innerWidth<768){
-            setOpen(false);
-        }
-        else{
         setImageSrc(imageSrc);
-        setOpen(true);}
+        setOpen(true);
     };
   
     const handleClose = () => {
       setOpen(false);
     };
-
-    const body = (
-        <div style={modalStyle} className={classes.paper}>
-          <img className={classes.media} src={imageSrc} alt={apiData.apiData.prj[0].projectName}></img>
-        </div>
-      );
+      
         if (loading.isLoading) {
             return (
             <>
             <Header />
-            <Loader messahe={loading.loadingMessage}/>
+            <Loader message={loading.loadingMessage}/>
             </>
             )
         } else {
     return (
         <React.Fragment>
             <Header />
-            <Container>
-                {/* ------------------------------------------------ Header ------------------------------------------------ */}
-                <header>
-                    <MetaTags>
-                        <title>{pageData.title}</title>
-                        <meta name="description" content={pageData.description} />
-                        <meta property="og:title" content={pageData.title} />
-                        {/* <meta property="og:image" content="path/to/image.jpg" /> */}
-                    </MetaTags>
-                </header>
-                 {/* ------------------------------------------------ Body ------------------------------------------------ */}
-                <section>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
+            <Container maxWidth={'md'}>
+                
+                <Grid container direction="row" justifyContent="center" alignItems="center">
+                        <Grid item xs={10}>
                             <Typography variant='h4' component='h1' align='left' className={classes.header}>
-                               Project Name: {apiData.apiData.prj[0].projectName}
+                               {projectData.project.project_title}
                             </Typography>
                         </Grid>
-                        <Grid item xs={12}>
-                        <Typography variant='h4' component='h1' align='left' className={classes.techStack}>
-                               Tech Stack
-                            </Typography>
-                            {apiData.apiData.prj[0].techStack.map((data) => {
-                                
+                        <Grid item xs={10}>
+                            {projectData.projectTechStack.map((data) => {
                                 return(
-                                <Chip color="primary" label={data} className={classes.chips}/>
+                                <Chip key={data.id} color="primary" label={data.tech_name} className={classes.chips}/>
                                 );
                         })}
                         </Grid>
-                        <Grid item xs={12} className={classes.heading}>
-                            <Typography variant='h4' component='h2' align='left' className={classes.projectOverview}>
-                                Project Overview
+                        <Grid item xs={10} className={classes.heading}>
+                            <Typography variant='h5' component='h2' align='left' className={classes.projectOverview}>
+                                Summary
                             </Typography>
                         </Grid>
-                        <Grid item xs={12}>
+                        <Grid item xs={10}>
                             <Typography variant='body2' align='left'>
-                                {apiData.apiData.prj[0].projectDescription}
+                                {projectData.project.description}
                             </Typography>
                         </Grid>
-                        <Grid item xs={12}>
-                            <Typography variant='h4' component='h2' align='left' className={classes.projectOverview}>
-                                The Project
+                        <Grid item xs={10}>
+                            <Typography variant='h5' component='h2' align='left' className={classes.projectOverview}>
+                                About The Project
                             </Typography>
                         </Grid>
-                        <Grid item xs={12}>
+                        <Grid item xs={10}>
                             <Typography variant='body2' align='left'>
-                                {apiData.apiData.prj[0].projectContent}
+                                {projectData.project.content}
                             </Typography>
                         </Grid>
-                        <Grid item xs={12}>
-                            <Button component='a' href={apiData.apiData.prj[0].projectUrl} target="_blank" variant="contained" color="primary" className={classes.button}>
-                                Live Link
+                        <Grid item xs={10}>
+                        <Button component='a' href={projectData.project.project_repo} target="_blank" variant="contained" color="primary" className={classes.button}>
+                                Project Repo
+                            </Button>
+                            <Button component='a' href={projectData.project.project_link} target="_blank" variant="outlined" color="primary" className={classes.button}>
+                                Live Project
                             </Button>
                         </Grid>
-                        <Modal open={open} onClose={handleClose} aria-labelledby="simple-modal-title" aria-describedby="simple-modal-description">{body}</Modal>
+                        <Grid container direction="row" justifyContent="center" alignItems="center">
+                        </Grid>
+                        <Dialog open={open} onClose={handleClose} >
+                        <img className={classes.media} src={imageSrc} alt={projectData.project.project_title}/>
+                        </Dialog>
+                        
                         <Grid item xs={12}>
                         <Typography variant='h4' component='h2' align='center' className={classes.photosHeading}>
                                Photos
                             </Typography>
                         </Grid>
-                        {apiData.apiData.img.map((image) => (
-                        <Grid item xs={12} sm={6} md={4}>
-                            <GridList cellHeight={160} className={classes.imageGrid}>
+                        <Grid item xs={10}>
+                        <ImageList rowHeight={280} className={classes.imageGrid}>
+                        {projectData.projectImages.map((image) => (
+                        
                             
-                                <GridListTile key={image.img}>
-                                    <img src={image.portfolioProjectImage} alt={apiData.apiData.prj[0].projectName} onClick={(src) => handleOpen(image.portfolioProjectImage)}/>
-                                </GridListTile>
-                            </GridList>
-                        </Grid>
+                                <ImageListItem key={image.id}>
+                                    <img src={image.image_url} alt={projectData.project.project_title} onClick={(src) => handleOpen(image.image_url)} />
+                                </ImageListItem>
                         ))}
-                    </Grid>
-                </section>
-                <Footer/>
+                        </ImageList>
+                        </Grid>
+                        </Grid>
+                
             </Container>
+            <Footer/>
         </React.Fragment>
 
     );
