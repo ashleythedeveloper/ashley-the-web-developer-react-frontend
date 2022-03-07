73e81d34c20abc1118f1e9d977a3ea5285e7cdec
ashleythewebdeveloper <ashley@harwoodslawncare.com.au>
2022-03-07 19:07:00 +1100

    Added an avatar image above the here heading and changed the hero heading text.

--- src/components/static_pages/About.js
+++ src/components/static_pages/About.js
@@ -10,8 +10,19 @@ import Footer from '../global/Footer';
 const useStyles = makeStyles((theme) => ({
     heroHeading: {
         fontSize: "3rem",
-        paddingTop: theme.spacing(20),
+        
     },
+    profileImage: {
+        borderRadius: '50%',
+    },
+    heroSection: {
+        marginTop: theme.spacing(20),
+    },
+    heroSectionImageContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        paddingBottom: theme.spacing(3)
+    }
 
 })
 );
@@ -31,9 +42,12 @@ const About = () => {
             <meta property="og:title" content="About | Ashley The Web Developer" />
             {/* <meta property="og:image" content="path/to/image.jpg" /> */}
           </MetaTags>
-            <Grid item xs={12}>
-                <Typography variant="h1" component="h1" align="center" className={classes.heroHeading}>
-                    About
+            <Grid item xs={12} className={classes.heroSection}>
+                <Grid item xs={12} className={classes.heroSectionImageContainer}>
+            <img src='./profileImage.png' width={150} height={150} className={classes.profileImage} />
+            </Grid>
+                <Typography variant="h2" component="h1" align="center" className={classes.heroHeading}>
+                    About Me
                 </Typography>
             </Grid>
             <Grid align="center" item xs={12}>
