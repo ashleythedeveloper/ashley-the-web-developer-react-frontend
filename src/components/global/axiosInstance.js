4ad413433e006d74704faa8884df968a9679ef64
ashleythewebdeveloper <ashley@harwoodslawncare.com.au>
2022-03-10 20:27:52 +1100

    Added url to local backend.

--- src/components/global/axiosInstance.js
+++ src/components/global/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const baseURL = 'https://api.ashleythewebdeveloper.com.au/api/';
+// const baseURL = 'https://api.ashleythewebdeveloper.com.au/api/';
+const baseURL = 'http://localhost:5000/api/';
+
 const axiosInstance = axios.create({
 	baseURL: baseURL,
 	timeout: 5000,
